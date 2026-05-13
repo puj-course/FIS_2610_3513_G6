af31a7f92dac09d452cc79c92376f3fe9837f45b
Jsanchez312 <140443285+Jsanchez312@users.noreply.github.com>
2026-05-13 17:50:35 -0500

    chore(test): agregar umbral cobertura 70% en Jest
    Closes #111

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,54 @@
 module.exports = {
   roots: ['<rootDir>/src/test/unit'],
-  
+
   testMatch: ['**/*.test.js'],
-  
+
   testEnvironment: 'node',
-  
+
   collectCoverage: true,
+
   coverageDirectory: 'src/test/coverage',
-  coverageReporters: ['html', 'json', 'lcov', 'text'],
-  
+
+  coverageReporters: [
+    'html',
+    'json',
+    'lcov',
+    'text',
+    'json-summary'
+  ],
+
+  coverageThreshold: {
+    global: {
+      lines: 70,
+      functions: 70,
+      branches: 60,
+      statements: 70
+    }
+  },
+
+  collectCoverageFrom: [
+    'unimercs-backend/*.js',
+    'unimercs-backend/routes/**/*.js',
+    'unimercs-backend/models/**/*.js',
+    'unimercs-backend/middleware/**/*.js',
+
+    '!**/node_modules/**',
+    '!src/test/**'
+  ],
+
   reporters: [
     'default',
     ['jest-junit', {
       outputDirectory: 'src/test/reports',
-      outputName: 'junit.xml'
+      outputName: 'junit.xml',
+      classNameTemplate: '{classname}',
+      titleTemplate: '{title}'
     }]
   ],
-  
+
   testTimeout: 10000,
-  
+
   clearMocks: true,
-  
+
   verbose: true
 };
