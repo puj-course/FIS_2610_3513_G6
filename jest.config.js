5d3609fe0312f9f17a85fb31e6cbd0c5cfaf5f99
Jsanchez312 <140443285+Jsanchez312@users.noreply.github.com>
2026-05-18 18:32:12 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src/test/unit'],
   testMatch: ['**/*.test.js'],
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
 
   collectCoverage: true,
   coverageDirectory: 'src/test/coverage',
